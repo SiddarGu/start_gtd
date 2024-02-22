fe94e3013fdeb39a9ebe68c566a24c3f5f959dbd
SiddarGu <fenggu2000@gmail.com>
2024-02-22 15:51:31 -0500

    fix: ip

--- src/MainPage.jsx
+++ src/MainPage.jsx
@@ -8,7 +8,7 @@ const MainPage = () => {
     useEffect(() => {
         // Fetch data from the backend
         axios
-            .get("http://192.168.1.78:5000/getdata")
+            .get("http://71.255.243.120/getdata")
             .then((response) => {
                 setData(response.data);
             })
